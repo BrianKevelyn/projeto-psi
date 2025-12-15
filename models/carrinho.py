cf88f98fcb3ab3a9bc5f29401785841b1321e208
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-12-15 15:17:04 -0300

    carrinho de compras

--- models/carrinho.py
+++ models/carrinho.py
@@ -1,6 +1,8 @@
 from database.connection import db
 
 class Carrinho(db.Model):
-    __tablename__ = 'tb_carrinho'
-    car_id = db.Column(db.Integer, primary_key=True)
-    
+    __tablename__ = 'tb_item_carrinho'
+    item_id = db.Column(db.Integer, primary_key=True)
+    livro_id = db.Column(db.Integer, db.ForeignKey('tb_livro.livro_id'), nullable=False, unique=True)
+    quantidade = db.Column(db.Integer, nullable=False, default=1)
+    preco_unitario = db.Column(db.Float, nullable=False)
